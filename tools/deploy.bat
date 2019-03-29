7616010cd74be323757e8327dfce378903f3ec09
Martin Kühn <mkuehn1989@gmail.com>
2019-03-29 23:57:23 +0100

    Added mwindows flag.

--- tools/deploy.bat
+++ tools/deploy.bat
@@ -1,9 +1,11 @@
-SET QT_BIN_DIR=C:\Dev\Qt\5.12.2\mingw73_64\bin
-SET PROFILE=debug
-SET SOURCE_DIR=cmake-build-debug-mingw-64
-SET TARGET_DIR=build
+SET ARCH=64
+SET PROFILE=release
+SET QT_BIN_DIR=C:\Dev\Qt\5.12.2\mingw73_%ARCH%\bin
+SET SOURCE_DIR=build\cmake-build-%PROFILE%-mingw-%ARCH%\PasteAndElaste.exe
+SET TARGET_DIR=bin
 
 SET PATH=%QT_BIN_DIR%;%PATH%
 
 cd ..
-windeployqt --%PROFILE% %SOURCE_DIR%
+windeployqt --%PROFILE% "%SOURCE_DIR%"
+cd tools
